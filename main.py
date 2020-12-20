37d188a3cdb376f16ae4b216157970763998b567
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2020-12-20 20:25:00 +0100

    update
    - Updated settitle method.
    - Now it should work on linux too.

--- main.py
+++ main.py
@@ -24,9 +24,13 @@ class Main:
         else:
             print("\n") * 120
 
-
-    def SetTitle(self,title_name:str):
-        system("title {0}".format(title_name))
+    def SetTitle(self,title:str):
+        if name == 'posix':
+            stdout.write(f"\x1b]2;{title}\x07")
+        elif name in ('ce', 'nt', 'dos'):
+            system(f'title {title}')
+        else:
+            stdout.write(f"\x1b]2;{title}\x07")
 
     def GetRandomUserAgent(self):
         useragents = self.ReadFile('useragents.txt','r')
